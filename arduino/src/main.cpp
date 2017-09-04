3f717ee673cd031e01f5bb34a4b54f808867fcaf
Eric van Dijken <eric@team-moki.nl>
2017-09-04 12:02:44 +0000

    added 2fase hover

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -22,25 +22,41 @@
 
 */
 
-#include "main.h"
+#
+include "main.h"
 
+//
+// Serial vars
+//
 String serial_command = "";
 String command = "";
 String value_string = "";
 int value = 0;
 boolean command_complete = false;
 
+//
+// Tiemout vars
+//
 int sensor_time = 0;
 int motor_time = 0;
 int ping_time = 0;
 int pong_time = 0;
-int power = 0;
 
+//
+// Pilot vars
+//
 int pilot = 0;
 int pilot_heading = 0;
+//
+// Hover vars
+//
 int hover = 0;
 float hover_depth = 0;
 
+//
+// Motor vars
+//
+int power = 0;
 struct motor_power {
   float m1;
   float m2;
@@ -58,11 +74,11 @@ void setup() {
   camera_setup();
   sensor_setup();
 
-// Init serial communication
+  // Init serial communication
   serial_command.reserve(200);
   Serial.begin(115200);
 
-// Wait for Serial port to be available
+  // Wait for Serial port to be available
   while ((timeout <= millis() + 10000)) {
     if (Serial) {
       Serial.println("ARDUINO READY");
@@ -73,8 +89,8 @@ void setup() {
       timeout = millis();
     }
   }
-//  if (timeout > 0) {
-//  }
+  //  if (timeout > 0) {
+  //  }
 
 }
 
@@ -123,76 +139,82 @@ void sensor_loop() {
 }
 
 void hover_loop() {
-//
-// Hover
-//
-      if (hover == true) {
-        if (depth_get() > hover_depth+1) {
-          motor_up(30);
-	Serial.print("Motor_5:");
-	Serial.println(power);
-	Serial.print("Motor_6:");
-	Serial.println(power);
-        } else if (depth_get() < hover_depth-1) {
-          motor_dive(30);
-	Serial.print("Motor_5:");
-	Serial.println(power);
-	Serial.print("Motor_6:");
-	Serial.println(power);
-        } else {
-          motor_stop();
-	Serial.print("Motor_5:");
-	Serial.println(0);
-	Serial.print("Motor_6:");
-	Serial.println(0);
-        }
-      }
+  //
+  // Hover
+  //
+  if (depth_get() > hover_depth) {
+    motor_up(20);
+    Serial.print("Motor_5:");
+    Serial.println(power);
+    Serial.print("Motor_6:");
+    Serial.println(power);
+  } else if (depth_get() > hover_depth + 10) {
+    motor_up(30);
+    Serial.print("Motor_5:");
+    Serial.println(power);
+    Serial.print("Motor_6:");
+    Serial.println(power);
+  } else if (depth_get() < hover_depth) {
+    motor_dive(20);
+    Serial.print("Motor_5:");
+    Serial.println(power);
+    Serial.print("Motor_6:");
+    Serial.println(power);
+  } else if (depth_get() < hover_depth - 10) {
+    motor_dive(30);
+    Serial.print("Motor_5:");
+    Serial.println(power);
+    Serial.print("Motor_6:");
+    Serial.println(power);
+  } else {
+    motor_stop();
+    Serial.print("Motor_5:");
+    Serial.println(0);
+    Serial.print("Motor_6:");
+    Serial.println(0);
+  }
 }
+
 void pilot_loop() {
-//
-// Pilot
-//
-      if (pilot == true) {
-//	Serial.println("Debug_Pilot");
-        if (imu_heading() > pilot_heading+1) {
-//	  Serial.println("Debug_Pilot_Left");
-          motor_left(10);
-	Serial.print("Motor_1:");
-	Serial.println(power);
-	Serial.print("Motor_2:");
-	Serial.println(power);
-	Serial.print("Motor_3:");
-	Serial.println(power);
-	Serial.print("Motor_4:");
-	Serial.println(power);
-        } else if (imu_heading() < pilot_heading-1) {
-//	  Serial.println("Debug_Pilot_Right");
-          motor_right(10);
-	Serial.print("Motor_1:");
-	Serial.println(power);
-	Serial.print("Motor_2:");
-	Serial.println(power);
-	Serial.print("Motor_3:");
-	Serial.println(power);
-	Serial.print("Motor_4:");
-	Serial.println(power);
-        } else {
-          motor_stop();
-	Serial.print("Motor_1:");
-	Serial.println(0);
-	Serial.print("Motor_2:");
-	Serial.println(0);
-	Serial.print("Motor_3:");
-	Serial.println(0);
-	Serial.print("Motor_4:");
-	Serial.println(0);
-        }
-      }
+  //
+  // Pilot
+  //
+  if (imu_heading() > pilot_heading + 1) {
+    motor_left(10);
+    Serial.print("Motor_1:");
+    Serial.println(power);
+    Serial.print("Motor_2:");
+     Serial.println(power);
+    Serial.print("Motor_3:");
+    Serial.println(power);
+    Serial.print("Motor_4:");
+    Serial.println(power);
+  } else if (imu_heading() < pilot_heading - 1) {
+    motor_right(10);
+    Serial.print("Motor_1:");
+    Serial.println(power);
+    Serial.print("Motor_2:");
+    Serial.println(power);
+    Serial.print("Motor_3:");
+    Serial.println(power);
+    Serial.print("Motor_4:");
+    Serial.println(power);
+  } else {
+    motor_stop();
+    Serial.print("Motor_1:");
+    Serial.println(0);
+    Serial.print("Motor_2:");
+    Serial.println(0);
+    Serial.print("Motor_3:");
+    Serial.println(0);
+    Serial.print("Motor_4:");
+    Serial.println(0);
+  }
 }
 
 void motor_loop() {
   motor_time = millis();
-//  motor_stop();
+  //  motor_stop();
 }
 
 void pingpong_loop() {
@@ -203,210 +225,214 @@ void pingpong_loop() {
 }
 
 void loop() {
-//
-// Run at "fixed" times
-//
+  //
+  // Run at "fixed" times
+  //
   int time = millis();
-//
-// Every 500 μs run the sensor loop
-//
+  //
+  // Every 500 μs run the sensor loop
+  //
   if ((time >= (sensor_time + 500)) || (time < sensor_time)) {
     sensor_loop();
   }
-//
-// Every 2500 μs run the ping loop
-//
+  //
+  // Every 2500 μs run the ping loop
+  //
   if (time >= (pong_time + 2500)) {
     pingpong_loop();
   }
-//
-// Every 150 μs run the motor loop.
-//
+  //
+  // Every 150 μs run the motor loop.
+  //
   if ((time >= (motor_time + 150)) || (time < motor_time)) {
-    pilot_loop();
-    hover_loop();
+    if (pilot == true) {
+      pilot_loop();
+    }
+    if (hover == true) {
+      hover_loop();
+    }
     motor_loop();
   }
 
-//
-// Command
-//
-   if (command_complete) {
-      serial_command.trim();
-      int pos = serial_command.indexOf(':');
-      command = serial_command.substring(0,pos);
-      value_string = serial_command.substring(pos+1);
-      value = value_string.toInt();
+  //
+  // Command
+  //
+  if (command_complete) {
+    serial_command.trim();
+    int pos = serial_command.indexOf(':');
+    command = serial_command.substring(0, pos);
+    value_string = serial_command.substring(pos + 1);
+    value = value_string.toInt();
 
-      Serial.print("Command: ");
-      Serial.print(command);
-      Serial.print(" Value: ");
-      Serial.println(value);
+    Serial.print("Command: ");
+    Serial.print(command);
+    Serial.print(" Value: ");
+    Serial.println(value);
 
-      if (command == "ARM") {
-           motor_arm(true);
-      } else if (command == "PING") {
-         ping_time = millis();
-      } else if (command == "DISARM") {
-	   motor_stop();
-           motor_arm(false);
+    if (command == "ARM") {
+      motor_arm(true);
+    } else if (command == "PING") {
+      ping_time = millis();
+    } else if (command == "DISARM") {
+      motor_stop();
+      motor_arm(false);
 
-      } else if (command == "Stop") {
-           motor_stop();
-   	   Serial.print("Motor_1:");
-	   Serial.println(0);
-	   Serial.print("Motor_2:");
-	   Serial.println(0);
-	   Serial.print("Motor_3:");
-	   Serial.println(0);
-	   Serial.print("Motor_4:");
-	   Serial.println(0);
-	   Serial.print("Motor_5:");
-	   Serial.println(0);
-	   Serial.print("Motor_6:");
-	   Serial.println(0);
+    } else if (command == "Stop") {
+      motor_stop();
+      Serial.print("Motor_1:");
+      Serial.println(0);
+      Serial.print("Motor_2:");
+      Serial.println(0);
+      Serial.print("Motor_3:");
+      Serial.println(0);
+      Serial.print("Motor_4:");
+      Serial.println(0);
+      Serial.print("Motor_5:");
+      Serial.println(0);
+      Serial.print("Motor_6:");
+      Serial.println(0);
 
-      } else if (command == "Pilot") {
-           if (value >= 0) {
-             pilot = true;
-             pilot_heading = value;
-           } else {
-             pilot = false;
-           }
-      } else if (command == "Hover") {
-           if (value >= 0) {
-             hover = true;
-             hover_depth = value;
-           } else {
-             hover = false;
-           }
-      } else if (command == "Power") {
-           power = value;
-
-// Right Left Reverse Forward Strafe_r Strafe_l Dive Up
-      } else if (command == "Forward") {
-        motor_forward(power);
-//	Serial.println("Debug_Forward");
-	Serial.print("Motor_1:");
-	Serial.println(-power);
-	Serial.print("Motor_2:");
-	Serial.println(power);
-	Serial.print("Motor_3:");
-	Serial.println(power);
-	Serial.print("Motor_4:");
-	Serial.println(-power);
-        
-      } else if (command == "Reverse") {
-        motor_reverse(power);
-//  	Serial.println("Debug_Reverse");
-	Serial.print("Motor_1:");
-	Serial.println(power);
-	Serial.print("Motor_2:");
-	Serial.println(-power);
-	Serial.print("Motor_3:");
-	Serial.println(-power);
-	Serial.print("Motor_4:");
-	Serial.println(power);
-
-      } else if (command == "Right") {
-	motor_right(power);
-	Serial.print("Motor_1:");
-	Serial.println(power);
-	Serial.print("Motor_2:");
-	Serial.println(-power);
-	Serial.print("Motor_3:");
-	Serial.println(power);
-	Serial.print("Motor_4:");
-	Serial.println(-power);
-
-      } else if (command == "Left") {
-	motor_left(power);
-	Serial.print("Motor_1:");
-	Serial.println(-power);
-	Serial.print("Motor_2:");
-	Serial.println(power);
-	Serial.print("Motor_3:");
-	Serial.println(-power);
-	Serial.print("Motor_4:");
-	Serial.println(power);
-
-      } else if (command == "Dive") {
-	motor_dive(power);
-	Serial.print("Motor_5:");
-	Serial.println(power);
-	Serial.print("Motor_6:");
-	Serial.println(-power);
-
-      } else if (command == "Up") {
-	motor_up(power);
-	Serial.print("Motor_5:");
-	Serial.println(-power);
-	Serial.print("Motor_6:");
-	Serial.println(power);
-
-      } else if (command == "Strafe_r") {
-	motor_strafe_right(power);
-	Serial.print("Motor_1:");
-	Serial.println(power);
-	Serial.print("Motor_2:");
-	Serial.println(power);
-	Serial.print("Motor_3:");
-	Serial.println(power);
-	Serial.print("Motor_4:");
-	Serial.println(power);
-
-      } else if (command == "Strafe_l") {
-	motor_strafe_left(power);
-	Serial.print("Motor_1:");
-	Serial.println(-power);
-	Serial.print("Motor_2:");
-	Serial.println(-power);
-	Serial.print("Motor_3:");
-	Serial.println(-power);
-	Serial.print("Motor_4:");
-	Serial.println(-power);
-
-      } else if (command == "Roll_r") {
-	motor_roll_right(power);
-	Serial.print("Motor_5:");
-	Serial.println(-power);
-	Serial.print("Motor_6:");
-	Serial.println(power);
-
-      } else if (command == "Roll_l") {
-	motor_roll_left(power);
-	Serial.print("Motor_5:");
-	Serial.println(power);
-	Serial.print("Motor_6:");
-	Serial.println(-power);
-
-      } else if (command == "Light1") {
-         if (value == LIGHT1_OFF) {
-           light_off();
-         } else {
-           light_on();
-         }
-      } else if (command == "Light2") {
-         if (value == LIGHT1_OFF) {
-           light_off();
-         } else {
-           light_on();
-         }
-      } else if (command == "Camx") {
-         camera_movex(value);
-      } else if (command == "Camy") {
-         camera_movey(value);
+    } else if (command == "Pilot") {
+      if (value >= 0) {
+        pilot = true;
+        pilot_heading = value;
+      } else {
+        pilot = false;
       }
+    } else if (command == "Hover") {
+      if (value >= 0) {
+        hover = true;
+        hover_depth = value;
+      } else {
+        hover = false;
+      }
+    } else if (command == "Power") {
+      power = value;
 
-      serial_command = "";
-      command_complete = false;
+      // Right Left Reverse Forward Strafe_r Strafe_l Dive Up
+    } else if (command == "Forward") {
+      motor_forward(power);
+      //      Serial.println("Debug_Forward");
+      Serial.print("Motor_1:");
+      Serial.println(-power);
+      Serial.print("Motor_2:");
+      Serial.println(power);
+      Serial.print("Motor_3:");
+      Serial.println(power);
+      Serial.print("Motor_4:");
+      Serial.println(-power);
+
+    } else if (command == "Reverse") {
+      motor_reverse(power);
+      //      Serial.println("Debug_Reverse");
+      Serial.print("Motor_1:");
+      Serial.println(power);
+      Serial.print("Motor_2:");
+      Serial.println(-power);
+      Serial.print("Motor_3:");
+      Serial.println(-power);
+      Serial.print("Motor_4:");
+      Serial.println(power);
+
+    } else if (command == "Right") {
+      motor_right(power);
+      Serial.print("Motor_1:");
+      Serial.println(power);
+      Serial.print("Motor_2:");
+      Serial.println(-power);
+      Serial.print("Motor_3:");
+      Serial.println(power);
+      Serial.print("Motor_4:");
+      Serial.println(-power);
+
+    } else if (command == "Left") {
+      motor_left(power);
+      Serial.print("Motor_1:");
+      Serial.println(-power);
+      Serial.print("Motor_2:");
+      Serial.println(power);
+      Serial.print("Motor_3:");
+      Serial.println(-power);
+      Serial.print("Motor_4:");
+      Serial.println(power);
+
+    } else if (command == "Dive") {
+      motor_dive(power);
+      Serial.print("Motor_5:");
+      Serial.println(power);
+      Serial.print("Motor_6:");
+      Serial.println(-power);
+
+    } else if (command == "Up") {
+      motor_up(power);
+      Serial.print("Motor_5:");
+      Serial.println(-power);
+      Serial.print("Motor_6:");
+      Serial.println(power);
+
+    } else if (command == "Strafe_r") {
+      motor_strafe_right(power);
+      Serial.print("Motor_1:");
+      Serial.println(power);
+      Serial.print("Motor_2:");
+      Serial.println(power);
+      Serial.print("Motor_3:");
+      Serial.println(power);
+      Serial.print("Motor_4:");
+      Serial.println(power);
+
+    } else if (command == "Strafe_l") {
+      motor_strafe_left(power);
+      Serial.print("Motor_1:");
+      Serial.println(-power);
+      Serial.print("Motor_2:");
+      Serial.println(-power);
+      Serial.print("Motor_3:");
+      Serial.println(-power);
+      Serial.print("Motor_4:");
+      Serial.println(-power);
+
+    } else if (command == "Roll_r") {
+      motor_roll_right(power);
+      Serial.print("Motor_5:");
+      Serial.println(-power);
+      Serial.print("Motor_6:");
+      Serial.println(power);
+
+    } else if (command == "Roll_l") {
+      motor_roll_left(power);
+      Serial.print("Motor_5:");
+      Serial.println(power);
+      Serial.print("Motor_6:");
+      Serial.println(-power);
+
+    } else if (command == "Light1") {
+      if (value == LIGHT1_OFF) {
+        light_off();
+      } else {
+        light_on();
+      }
+    } else if (command == "Light2") {
+      if (value == LIGHT1_OFF) {
+        light_off();
+      } else {
+        light_on();
+      }
+    } else if (command == "Camx") {
+      camera_movex(value);
+    } else if (command == "Camy") {
+      camera_movey(value);
     }
+
+    serial_command = "";
+    command_complete = false;
+  }
 }
 
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read();
+    char inChar = (char) Serial.read();
     // add it to the inputString:
     serial_command += inChar;
     // if the incoming character is a newline, set a flag
@@ -416,4 +442,3 @@ void serialEvent() {
     }
   }
 }
-
